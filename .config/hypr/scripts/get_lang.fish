fad461608c3c9a5135fdcd89208d7ea0076287b4
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-09-10 14:56:49 +0300

    feat: add lazyvim extras, change keyboard layout module in hyprpanel to a new view and change name of language to human-readable

--- .config/hypr/scripts/get_lang.fish
+++ .config/hypr/scripts/get_lang.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/fish
-set -l lang (hyprctl devices -j | jq ".keyboards | .[] | select(.main==true) | .active_keymap")
+set lang (hyprctl devices -j | jq ".keyboards | .[] | select(.main==true) | .active_keymap")
 
-if echo $lang | grep -q -i ru
-    set short_lang ru
-else
-    set short_lang en
-end
+#if echo $lang | grep -q -i ru
+#    set short_lang RU
+#else
+#    set short_lang EN
+#end
 
-echo "{\"lang\": \"$short_lang\"}"
+echo "{\"lang\": $lang}"
