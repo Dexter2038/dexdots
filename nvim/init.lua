d0be5c0f0789ee928b35a380682dfc9d82bbdc35
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-09-26 21:30:16 +0300

    feat(nvim): make neovim transparent

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-
 vim.g.rustaceanvim = {
   server = {
     settings = {
@@ -14,3 +13,9 @@ vim.g.rustaceanvim = {
   },
 }
 
+require("transparent").clear_prefix("BufferLine")
+require("transparent").clear_prefix("NeoTree")
+require("transparent").clear_prefix("lualine")
+require("transparent").clear_prefix("snacks")
+require("transparent").clear_prefix("telescope")
+require("transparent").clear_prefix("lazy")
